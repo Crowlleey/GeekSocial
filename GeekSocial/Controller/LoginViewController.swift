3a1408d6c165294f2a2eeffa7b8a0b1b70fb6b01
George Gomes <georgegomees@gmail.com>
2018-03-16 22:48:10 -0300

    Add initial layout

--- GeekSocial/Controller/LoginViewController.swift
+++ GeekSocial/Controller/LoginViewController.swift
@@ -12,19 +12,11 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override var prefersStatusBarHidden: Bool{
+        return true
     }
 }
 
-class oi{
-    
-    func sum(_ a: Int, _ b: Int) -> Int {
-        return a + b
-    }
-    
-}
